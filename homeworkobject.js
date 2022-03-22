ed59f74c1041f20a6c80d5a36856735f4270c334
manel <95052544+sleepypanda2002@users.noreply.github.com>
2022-03-22 22:01:37 +0100

    Update homeworkobject.js

--- homeworkobject.js
+++ homeworkobject.js
@@ -11,17 +11,6 @@ var objecttwo = {
   end: "last",
 };
 
-/*const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-
-    if (keys1.length !== keys2.length) {
-        return false;
-    }
-    for (let key of keys1) {
-        if (obj1[key] !== obj2[key]) {
-            return false;
-        }
-    }*/
 function compare_objects(object1, object2) {
 var compare;
 if (object1===object2){
